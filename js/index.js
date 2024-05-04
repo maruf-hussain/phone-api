8202a2fc030040e591ed6a57960c4b12bb6aecb4
maruf-hussain <engmarufhussain@gmail.com>
2024-05-05 04:28:18 +0600

    phone search complete

--- js/index.js
+++ js/index.js
@@ -1,7 +1,41 @@
-const loadPhone = async() =>{
-const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+// https://api.restful-api.dev/objects
+const loadPhone = async(searchPhone) =>{
+const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchPhone}`);
 const data = await res.json();
-console.log(data);
+const phones = data.data;
+displayPhones(phones);
 
 }
-loadPhone();
+
+const displayPhones = phones =>{
+    // console.log(phones);
+    const phoneContainer = document.getElementById('phone-container');
+    phoneContainer.textContent = '';
+    phones.forEach(phone =>{
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card  p-4`;
+        phoneCard.innerHTML = `
+        <div class="card  bg-base-100 ">
+        <figure><img src="${phone.image}" alt="Iphone" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">${phone.phone_name}</h2>
+          <p>${phone.slug}</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>`
+      phoneContainer.appendChild(phoneCard);
+
+    })
+}
+
+// Search ................................................
+const handleSearch = () =>{
+   const searchFiele = document.getElementById('search-field');
+   const searchPhone = searchFiele.value;
+   searchFiele.value = '';
+  
+   loadPhone(searchPhone);
+}
+// loadPhone();
